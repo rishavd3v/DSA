b0d889567bd5d137a4ed419e9e34661d7b6e8433
Rishav <rishavraj4273@gmail.com>
2023-06-29 22:28:48 +0530

    modified

--- Bubble_Sort/src/Main.java
+++ Bubble_Sort/src/Main.java
@@ -9,12 +9,13 @@ public class Main {
     }
 
     static void bubblesort(int[] arr){
+        // Bubble Sort
+        // Select element at index '1' and then compare it with previous element and swap if necessary.
         for (int i = 0; i < arr.length ; i++) {
-            boolean swap = false;  // check if array is already sorted
+            boolean swap = false;  // to check if array is already sorted
 
             for (int j = 1; j < arr.length-i; j++) {
-
-                // above -i is used because after every iteration of i, 1 element will reach its correct position. So, we do not need to check it.
+                // above 'arr.length-i' is used because after every iteration of i, one element will reach its correct position. So, we do not need to check it.
 
                 if(arr[j-1]>arr[j]){
                     // swap
