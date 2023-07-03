ee952d50f76eaa2c152c745012ce070fbd07a517
Rishav <rishavraj4273@gmail.com>
2023-07-03 11:06:11 +0530

    Added ques#448

---
+++ Problems/Leetcode/src/FindAllMissing.java
@@ -0,0 +1,33 @@
+// Ques#448 - https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+public class FindAllMissing {
+
+
+    class Solution {
+        public List<Integer> findDisappearedNumbers(int[] arr) {
+            int i = 0;
+            while(i<arr.length){
+                int correct = arr[i]-1;
+                if(arr[i] != arr[correct]){
+                    //swap
+                    int temp = arr[i];
+                    arr[i] = arr[correct];
+                    arr[correct] = temp;
+                }
+                else{
+                    i++;
+                }
+            }
+            List<Integer> ans = new ArrayList<>();
+            for (i = 0; i < arr.length; i++) {
+                if(arr[i]!= i+1){
+                    ans.add(i+1);
+                }
+            }
+            return ans;
+        }
+    }
+
+
+
+}
