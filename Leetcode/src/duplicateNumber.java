31c8465aa9e6974d3cf8a77acda7da808a734c13
Rishav <rishavraj4273@gmail.com>
2023-07-04 21:01:46 +0530

    added question

---
+++ Leetcode/src/duplicateNumber.java
@@ -0,0 +1,54 @@
+
+//Ques#287 - https://leetcode.com/problems/find-the-duplicate-number/
+
+public class duplicateNumber {
+
+// Solution #1
+    class Solution {
+        public int findDuplicate(int[] arr) {
+            int i=0;
+            while(i<arr.length){
+                int correct = arr[i]-1;
+                if(arr[i] != arr[correct]){
+                    //swap
+                    int temp = arr[i];
+                    arr[i] = arr[correct];
+                    arr[correct] = temp;
+                }
+                else{
+                    i++;
+                }
+            }
+            return arr[arr.length-1];
+        }
+    }
+
+// Solution #2
+    class Solution {
+        public int findDuplicate(int[] arr) {
+            int i=0;
+            while(i<arr.length){
+                if(arr[i] != i+1){
+                    int correct = arr[i]-1;
+                    if(arr[i] != arr[correct]){
+                        //swap
+                        int temp = arr[i];
+                        arr[i] = arr[correct];
+                        arr[correct] = temp;
+                    }
+                    else{
+                        return arr[i];
+                    }
+                }
+                else{
+                    i++;
+                }
+            }
+            return -1;
+        }
+    }
+
+
+
+
+}
